b8899a01cec0212956e8375b0c1c3c66ea4e7c45
Tuan <gonhungho123@gmail.com>
2022-09-07 18:01:16 +0700

    update BMi app

--- app/src/main/java/com/example/vidu1/MainActivity.java
+++ app/src/main/java/com/example/vidu1/MainActivity.java
@@ -8,43 +8,54 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
-    private Button tinh;
-    private TextView BMI,Noti;
-    private EditText height,weight;
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        height = (EditText) findViewById(R.id.height);
-        weight = (EditText) findViewById(R.id.weight);
-        BMI = (TextView) findViewById(R.id.BMI);
-        Noti = (TextView) findViewById(R.id.Noti);
-        tinh = (Button) findViewById(R.id.tinh);
-        tinh.setOnClickListener(new View.OnClickListener() {
+        myButtonListenerMethod();
+    }
+    public void myButtonListenerMethod() {
+        Button button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                float average,calHeight,calWeight;
-
-                calHeight = Integer.parseInt(weight.getText().toString());
-                calWeight = Float.parseFloat(weight.getText().toString());
-                String str = String.valueOf(calWeight/(calHeight * calHeight));
-                BMI.setText(str);
-                average = calWeight/(calHeight * calHeight);
-                if(average >= 35) {
-                    Noti.setText("Nguoi beo phi do III");
-                } else if (average >= 30 && average < 35) {
-                    Noti.setText("Nguoi beo phi do II");
-                } else if (average >= 25 && average < 30) {
-                    Noti.setText("Nguoi beo phi do I");
-                } else if (average >= 18 && average < 25) {
-                    Noti.setText("Nguoi binh thuong");
-                } else if (average < 18) {
-                    Noti.setText("Nguoi gay");
-                } else Noti.setText("Bao loi");
+            public void onClick(View v) {
+                final EditText heightText = findViewById(R.id.height);
+                String heightStr = heightText.getText().toString();
+                double height = Double.parseDouble(heightStr);
+                double heightM = height/100;
+                final EditText weightText = findViewById(R.id.weight);
+                String weightStr = weightText.getText().toString();
+                double weight = Double.parseDouble(weightStr);
+                double BMI = (weight) / (heightM * heightM);
+                DecimalFormat df = new DecimalFormat("#.#");
+                double BMI_trimmed = Double.parseDouble(df.format(BMI));
+                final TextView BMIResult = findViewById(R.id.result);
+                BMIResult.setText(Double.toString(BMI_trimmed));
+                String BMI_Cat;
+                 if (BMI < 18){
+                    BMI_Cat = "Nguoi gay";
+                }
+                else if (BMI >=18 && BMI < 25){
+                    BMI_Cat = "nguoi binh thuong";
+                }
+                else if (BMI >= 25 && BMI < 30){
+                    BMI_Cat = "Nguoi beo phi cap do 1";
+                }
+                else if (BMI>=30 && BMI < 35 ){
+                    BMI_Cat = "Nguoi beo phi cap do 2";
+                }
+                else if (BMI>= 35){
+                    BMI_Cat = "Nguoi beo phi cap do 3";
+                }
+                else {
+                    BMI_Cat = "Khong xac dinh";
+                }
+                final TextView BMICategory = findViewById(R.id.bmiCat);
+                BMICategory.setText(BMI_Cat);
             }
         });
     }
-
-
 }
